9b09efe0c2c065e1de2deb6380e711e4a395a507
priyanshu953 <priyansh_24ds031@saitm.ac.in>
2024-12-03 15:51:30 +0530

    Add files via upload

---
+++ DAY 7/Index.js
@@ -0,0 +1,45 @@
+// console.log("----Welcome in js----");
+// const title = document.getElementById("title");
+// console.log(title);
+
+//title.innerText = "hii i'm Priyanshu";
+// getElementsByclassName=("desc");
+// console.log("desc");
+// desc[0].innerText = "my name is Priyanshu";
+// const heading = document;
+// getElementsByclassName("heading");
+
+// heading[0].console
+
+// //select 
+// consth1Ele = document.getElementsByClassName("title");
+
+// //new content variable
+// const newContent = "------12345------";
+
+// //assign Value to tag
+// // consth1Ele.innerText = newContent;
+// //            0    1   2   3,.....
+// // divEle = [div,div,div,div,....];
+
+// consth1Ele[1].innerText = "first div Tag";
+// consth1Ele[2].innerText = "second div Tag";
+// consth1Ele[3].innerText = "Third div Tag";
+// consth1Ele[4].innerText = "fourth div Tag";
+
+//Arry 
+//   // index : 0   1   2   3   4   5   6   7   8   9
+// const arr = [10, 9,  8,  7,  6,  5,  4,  3,  2,  1];
+
+//print Specific element of an array;
+//syntax : array_name : array_name.length
+
+// const firstElement = arr[0];
+
+//  Print Sum of all Element:
+//  Print sum of Odd number:
+//  Print Elements :divisible by 3 and 5:
+for(i = 0, i <= 9 , i++) {
+    
+ }
+
