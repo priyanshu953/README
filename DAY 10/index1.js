d5164b6501d05f76c242f2afcad714f9f903cf01
priyanshu953 <priyansh_24ds031@saitm.ac.in>
2024-12-06 15:56:42 +0530

    Add files via upload

---
+++ DAY 10/index1.js
@@ -0,0 +1,39 @@
+// const counter = document.getElementById("counter");
+
+// let count = 0;
+// function incrementValue() {
+//     count++;
+//     counter.innerText = count;
+// }
+// const counter = document.getElementById("counter");
+
+// let count = 10;
+// function decrementValue() {
+//     count--;
+//     counter.innerText = count;
+// }
+
+// try {
+//     const a = 5;
+// const b = 9;
+// console.log("----value of a----:");
+// a=6;
+// console.log("---value of a:---")
+// }
+// catch(error) {
+//     console.log("----value of b:----")
+// }
+// function myPromiseFun(res,rej) {
+//     let delay = 100000000;
+//     while(delay--);
+//     res("Hello World");
+// }
+const mypromise = new Promise();
+// console.log(myPromise);
+function solve(fun1,fun2) {
+    let n=999999;
+    
+
+}
+//setTimeout
+//setInterval
